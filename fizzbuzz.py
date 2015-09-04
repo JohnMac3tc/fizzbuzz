891c728bdf510edea4b47b1b365ed0233dfacae0
johnromac <john@theusic.com>
2015-09-04 14:22:12 +0000

    Initial Commit

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+for n in xrange(1, 100):
+    if n % 3 == 0 and n % 5 == 0:
+        print "fizzbuzz"
+    elif n % 5 == 0:
+        print "fizz"
+    elif n % 3 == 0:
+        print "buzz"
+    else:
+        print n
+    
+   
+   
+        
+   
+      
+            
+            
+     
